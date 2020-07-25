e3641502be19e2fb1e8f0dde5bda406e5467d861
Jastler <chirkovtaras@gmail.com>
2020-07-25 13:35:32 +0300

    add2

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-window.onscroll = function() {
-  var scrollElem = document.getElementById("scrollToTop");
-  if (document.body.scrollTop > document.documentElement.clientHeight) {
-     scrollElem.style.opacity = "0";
-  } else {
-      scrollElem.style.opacity = "1";
-  }
-}
-var timeOut;
-function goUp() {
-   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-   if(top > 0) {
-      window.scrollBy(0,-100);
-      timeOut = setTimeout('goUp()',20);
-   } else clearTimeout(timeOut);
+let nav_burder = document.querySelector(".burger");
+let menu = document.querySelector(".header-menu")
+let body = document.querySelector("body")
+
+nav_burder.onclick = function () {
+  nav_burder.classList.toggle('active');
+  menu.classList.toggle('active');
+  body.classList.toggle('lock')
 }
